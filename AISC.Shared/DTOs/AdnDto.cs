efb863bc6b9e696d42a06434b791feaea6ba8195
Rafael Estrada <rafael.estrada@ideact.se>
2022-10-10 09:51:45 -0500

    cambios en clase adn

--- AISC.Shared/DTOs/AdnDto.cs
+++ AISC.Shared/DTOs/AdnDto.cs
@@ -6,7 +6,7 @@ namespace ADN.Shared.DTOs
     public class AdnDto
     {
         public int Id { get; set; }
-        public string? Adn { get; set; }
+        public string? Adn1 { get; set; }
         public bool IsClon { get; set; }
     }
 }
